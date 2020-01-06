567ea6472fa9de4e1b2c047de7cc37e30b7a9242
antonboltnev <38510677+antonboltnev@users.noreply.github.com>
2020-01-06 22:11:34 +0300

    first component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
     <p>Hello Vue!</p>
+    <v-main-wrapper />
   </div>
 </template>
 
 <script>
+  import vMainWrapper from './components/v-main-wrapper'
 
 export default {
   name: 'app',
   components: {
-
+    vMainWrapper
   }
 }
 </script>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="v-main-wrapper">
+    <h1>{{catalog}}</h1>
+    <p>{{summa}}</p>
+    <button @click="increment">Press</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'v-main-wrapper', //vMainWrapper
+    props: {},
+    data() {
+      return {
+        title: 'Catalog',
+        a: 1,
+        b: 3
+      }
+    },
+    computed: {
+      catalog() {
+        return this.title + '!'
+      },
+      summa() {
+        return (this.a + this.b) + 'P'
+      }
+    },
+    methods: {
+      increment() {
+        this.b++
+      }
+    },
+    watch: {
+      summa() {
+        console.log(this.summa)
+      },
+      b() {
+        if (this.b > 10) {
+          console.log(this.b)
+        }
+      }
+    },
+    mounted() {
+      console.log('Hello i am alive!')
+    },
+  }
+</script>
+
+<style>
+  .v-main-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-width: 900px;
+    margin: 0 auto;
+  }
+</style>
+
