adbb9b4f9b44a1fc44d07918d1f4b59f005232c2
antonboltnev <38510677+antonboltnev@users.noreply.github.com>
2020-01-07 16:58:49 +0300

    lesson 3

--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -1,20 +1,36 @@
 <template>
   <div class='v-catalog-item'>
-    <img src="" alt="img">
-    <p>Item 1</p>
-    <p>Price: 100</p>
-    <button>Add to cart</button>
+    <img class="v-catalog-item__image" :src=" require('../assets/images/' + product_data.image) " alt="img">
+    <p class="v-catalog-item__name">{{product_data.name}}</p>
+    <p class="v-catalog-item__price">Price: {{product_data.price}} Р.</p>
+    <button
+        class="v-catalog-item__add_to_cart_btn btn"
+        @click="sendDataToParent"
+    >Add to cart
+    </button>
   </div>
 </template>
 
 <script>
   export default {
     name: "v-catalog-item",
-    props: {},
+    props: {
+      product_data: {
+        type: Object,
+        default() {
+          return {}
+        }
+      }
+    },
     data() {
       return {}
     },
-    computed: {}
+    computed: {},
+    methods: {
+      sendDataToParent() {
+        this.$emit('sendDataToParent', this.product_data.article)
+      }
+    }
   }
 </script>
 
@@ -22,7 +38,10 @@
   .v-catalog-item {
     flex-basis: 25%;
     box-shadow: 0 0 8px 0 #e0e0e0;
-    padding: 16px;
-    margin-bottom: 16px;
+    padding: $padding*2;
+    margin-bottom: $margin*2;
+    &__image {
+      width: 100px;
+    }
   }
 </style>
--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -2,12 +2,12 @@
   <div class='v-catalog'>
     <h1>Catalog</h1>
     <div class="v-catalog__list">
-      <v-catalog-item />
-      <v-catalog-item />
-      <v-catalog-item />
-      <v-catalog-item />
-      <v-catalog-item />
-      <v-catalog-item />
+      <v-catalog-item
+          v-for="product in products"
+          :key="product.article"
+          :product_data="product"
+          @sendDataToParent="showChildArticleInConsole"
+      />
     </div>
   </div>
 </template>
@@ -22,9 +22,59 @@
     },
     props: {},
     data() {
-      return {}
+      return {
+        products: [
+          {
+            image: "1.jpg",
+            name: "T-shirt 1",
+            price: 100,
+            article: "T1",
+            available: true
+          },
+          {
+            image: "2.jpg",
+            name: "T-shirt 2",
+            price: 150,
+            article: "T2",
+            available: true
+          },
+          {
+            image: "3.jpg",
+            name: "T-shirt 3",
+            price: 200,
+            article: "T3",
+            available: false
+          },
+          {
+            image: "4.jpg",
+            name: "T-shirt 4",
+            price: 300,
+            article: "T4",
+            available: true
+          },
+          {
+            image: "5.jpg",
+            name: "T-shirt 5",
+            price: 500,
+            article: "T5",
+            available: false
+          },
+          {
+            image: "6.jpeg",
+            name: "T-shirt 6",
+            price: 700,
+            article: "T6",
+            available: true
+          }
+        ]
+      }
     },
-    computed: {}
+    computed: {},
+    methods: {
+      showChildArticleInConsole(data) {
+        console.log(data)
+      }
+    }
   }
 </script>
 
